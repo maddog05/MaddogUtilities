f85cf3f9721fc2cc30a2c6388ca096ba0576735a
andree <atorres@inticousa.com>
2017-08-25 17:45:55 -0500

    ADD SquareImageView tiene booleano para controlar con que dimension se realice el cuadrado

--- maddogutilities/src/main/java/com/maddog05/maddogutilities/view/SquareImageView.java
+++ maddogutilities/src/main/java/com/maddog05/maddogutilities/view/SquareImageView.java
@@ -1,14 +1,20 @@
 package com.maddog05.maddogutilities.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+
+import com.maddog05.maddogutilities.R;
 
 /**
  * Created by andreetorres on 13/05/17.
  */
 
 public class SquareImageView extends AppCompatImageView {
+
+    private boolean useHorizontal;
+
     public SquareImageView(Context context) {
         super(context);
     }
@@ -19,11 +25,19 @@ public class SquareImageView extends AppCompatImageView {
 
     public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.SquareImageView);
+        useDimensionHorizontal(attributes.getBoolean(R.styleable.SquareImageView_siv_useDimensionHorizontal, true));
+        attributes.recycle();
+    }
+
+    private void useDimensionHorizontal(boolean useHorizontal) {
+        this.useHorizontal = useHorizontal;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+        int dimension = useHorizontal ? getMeasuredWidth() : getMeasuredHeight();
+        setMeasuredDimension(dimension, dimension);
     }
 }
